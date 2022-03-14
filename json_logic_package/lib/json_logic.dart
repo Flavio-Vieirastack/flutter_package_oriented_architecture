fb7737c7c96c3f7cce7481511a5734aed3bd6b5c
Flavio Vieira <flavioemerson1@outlook.com>
2022-03-14 15:05:16 -0300

    feito pequeno refactor

--- json_logic_package/lib/json_logic.dart
+++ json_logic_package/lib/json_logic.dart
@@ -1,5 +1,5 @@
 library json_logic;
 
 export 'data/DTOs/posts_dto.dart';
-export 'domain/usecases/listOfPosts/list_of_posts_use_case.dart'; //ver esse cara
+export 'domain/usecases/listOfPosts/list_of_posts_use_case.dart';
 export 'core/adapter/inject.dart';
